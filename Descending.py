1046cf5ed91d114aa4be211fc8c17322d9368430
Azii <violetlovato05@gmail.com>
2021-11-22 22:22:21 +0800

    After many changes and testing, It's finally done

--- Descending.py
+++ Descending.py
@@ -6,11 +6,11 @@ def arrange():
     if Value_1 > Value_2 and Value_1 > Value_3 and Value_1 > Value_4:
         if Value_2 > Value_3 and Value_3 > Value_4:
             print (f"The order of the number is {Value_1}, {Value_2}, {Value_3}, {Value_4}.")
-        elif Value_2 > Value_3 and Value_3 < Value_4:
+        elif Value_2 > Value_3 and Value_2 > Value_4:
             print (f"The order of the number is {Value_1}, {Value_2}, {Value_4}, {Value_3}.")
         elif Value_2 < Value_3 and Value_2 > Value_4:
             print (f"The order of the number is {Value_1}, {Value_3}, {Value_2}, {Value_4}.")
-        elif Value_2 < Value_3 and Value_2 < Value_4:
+        elif Value_2 < Value_3 and Value_3 > Value_4:
             print (f"The order of the number is {Value_1}, {Value_3}, {Value_4}, {Value_2}.")
         elif Value_2 < Value_3 and Value_3 < Value_4:
             print (f"The order of the number is {Value_1}, {Value_4}, {Value_3}, {Value_2}.")
@@ -45,5 +45,20 @@ def arrange():
         else:
             Value_1 < Value_2 and Value_2 < Value_4
             print (f"The order of the number is {Value_3}, {Value_4}, {Value_2}, {Value_1}.")
+    else:
+        Value_4 > Value_1 and Value_4 > Value_2 and Value_4 > Value_3
+        if Value_1 > Value_2 and Value_2 > Value_3:
+            print (f"The order of the number is {Value_4}, {Value_1}, {Value_2}, {Value_3}.")
+        elif Value_1 > Value_2 and Value_1 > Value_3:
+            print (f"The order of the number is {Value_4}, {Value_1}, {Value_3}, {Value_2}.")
+        elif Value_1 < Value_2 and Value_1 > Value_3:
+            print (f"The order of the number is {Value_4}, {Value_2}, {Value_1}, {Value_3}.")
+        elif Value_1 < Value_2 and Value_2 > Value_3:
+            print (f"The order of the number is {Value_4}, {Value_2}, {Value_3}, {Value_1}.")
+        elif Value_1 > Value_2 and Value_1 < Value_3:
+            print (f"The order of the number is {Value_4}, {Value_3}, {Value_1}, {Value_2}.")
+        else:
+            Value_1 < Value_2 and Value_2 < Value_3
+            print (f"The order of the number is {Value_4}, {Value_3}, {Value_2}, {Value_1}.")
 
 arrange()
