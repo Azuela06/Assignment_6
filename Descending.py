2608f0895c74df3eba273b3a69400914b9aa4077
Azii <violetlovato05@gmail.com>
2021-11-22 19:48:34 +0800

    The first conditional worked!

---
+++ Descending.py
@@ -0,0 +1,21 @@
+def arrange():
+    Value_1 = int(input("Give a random number: "))
+    Value_2 = int(input("Give a random number: "))
+    Value_3 = int(input("Give a random number: "))
+    Value_4 = int(input("Give a random number: "))
+    if Value_1 > Value_2 and Value_1 > Value_3 and Value_1 > Value_4:
+        if Value_2 > Value_3 and Value_3 > Value_4:
+            print (f"The order of the number is {Value_1}, {Value_2}, {Value_3}, {Value_4}.")
+        elif Value_2 > Value_3 and Value_3 < Value_4:
+            print (f"The order of the number is {Value_1}, {Value_2}, {Value_4}, {Value_3}.")
+        elif Value_2 < Value_3 and Value_2 > Value_4:
+            print (f"The order of the number is {Value_1}, {Value_3}, {Value_2}, {Value_4}.")
+        elif Value_2 < Value_3 and Value_2 < Value_4:
+            print (f"The order of the number is {Value_1}, {Value_3}, {Value_4}, {Value_2}.")
+        elif Value_2 < Value_3 and Value_3 < Value_4:
+            print (f"The order of the number is {Value_1}, {Value_4}, {Value_3}, {Value_2}.")
+        else:
+            Value_2 > Value_3 and Value_2 < Value_4
+            print (f"The order of the number is {Value_1}, {Value_4}, {Value_2}, {Value_3}.")
+
+arrange()
