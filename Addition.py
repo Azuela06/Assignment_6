23e9f3eb0216225a1eaa915a36427670a2d52977
Azii <violetlovato05@gmail.com>
2021-11-23 23:47:41 +0800

    I'm still figuring out how this'll work

--- Addition.py
+++ Addition.py
@@ -1,8 +1,8 @@
 import random 
 
 def Quiz_add():
-    q1 = 1
-    if q1 == 1:
+    if Q1 == 1:
+        print("The first question is ")
         score = 0
         a = random.randint(0,99)
         b = random.randint(0,99)
@@ -13,5 +13,126 @@ def Quiz_add():
         else:
             score += 0
         return score
+        q2 = print("The second question is ")
+        Q2 = 2
+    elif Q2 == 2:
+        print("The second question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+    elif Q3 == 3:
+        print("The third question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+    elif Q4 == 4:
+        print("The fourth question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+    elif Q5 == 5:
+        print("The fifth question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+    elif Q6 == 6:
+        print("The sixth question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+    elif Q7 == 7:
+        print("The seventh question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+    elif Q8 == 8:
+        print("The eighth question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+    elif Q9 == 9:
+        print("The ninth question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+    else:
+        Q10 == 10
+        print("The tenth question is ")
+        score = 0
+        a = random.randint(0,99)
+        b = random.randint(0,99)
+        Question = a + b
+        answer= int(input(f"the answer in {a} + {b} is: "))
+        if answer == Question:
+            score += 1
+        else:
+            score += 0
+        return score
+Q1 = 1
+Q2 = 2
+Q3 = 3
+Q4 = 4
+Q5 = 5
+Q6 = 6
+Q7 = 7
+Q8 = 8
+Q9 = 9
+Q10 = 10
 
-print(Quiz_add())
+Quiz_add()
