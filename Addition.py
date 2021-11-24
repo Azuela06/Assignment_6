f90cff0ca9135212445411c64f86c77715da9a84
Azii <violetlovato05@gmail.com>
2021-11-24 14:29:01 +0800

    I've had multiple revisions like too many

--- Addition.py
+++ Addition.py
@@ -1,33 +1,33 @@
 import random 
 
-def Quiz_add():
-    if Q1 == 1:
-        print("The first question is ")
-        score = 0
-        a = random.randint(0,99)
-        b = random.randint(0,99)
-        Question = a + b
-        answer= int(input(f"the answer in {a} + {b} is: "))
-        if answer == Question:
-            score += 1
-        else:
-            score += 0
-        return score
-        q2 = print("The second question is ")
-        Q2 = 2
-    elif Q2 == 2:
-        print("The second question is ")
-        score = 0
-        a = random.randint(0,99)
-        b = random.randint(0,99)
-        Question = a + b
-        answer= int(input(f"the answer in {a} + {b} is: "))
-        if answer == Question:
-            score += 1
-        else:
-            score += 0
-        return score
-    elif Q3 == 3:
+
+def Number_1():
+    print("The first question is ")
+    score = 0
+    a = random.randint(0,99)
+    b = random.randint(0,99)
+    Question = a + b
+    answer= int(input(f"the answer in {a} + {b} is: "))
+    if answer == Question:
+        score += 1
+    else:
+        score += 0
+    return score
+
+def Number_2():
+    print("The second question is ")
+    score = 0
+    a = random.randint(0,99)
+    b = random.randint(0,99)
+    Question = a + b
+    answer= int(input(f"the answer in {a} + {b} is: "))
+    if answer == Question:
+        score += 1
+    else:
+        score += 0
+    return score
+
+def Number_3():
         print("The third question is ")
         score = 0
         a = random.randint(0,99)
@@ -39,7 +39,8 @@ def Quiz_add():
         else:
             score += 0
         return score
-    elif Q4 == 4:
+
+def Number_4():
         print("The fourth question is ")
         score = 0
         a = random.randint(0,99)
@@ -51,7 +52,8 @@ def Quiz_add():
         else:
             score += 0
         return score
-    elif Q5 == 5:
+
+def Number_5():
         print("The fifth question is ")
         score = 0
         a = random.randint(0,99)
@@ -63,7 +65,8 @@ def Quiz_add():
         else:
             score += 0
         return score
-    elif Q6 == 6:
+
+def Number_6():
         print("The sixth question is ")
         score = 0
         a = random.randint(0,99)
@@ -75,7 +78,8 @@ def Quiz_add():
         else:
             score += 0
         return score
-    elif Q7 == 7:
+
+def Number_7():
         print("The seventh question is ")
         score = 0
         a = random.randint(0,99)
@@ -87,7 +91,8 @@ def Quiz_add():
         else:
             score += 0
         return score
-    elif Q8 == 8:
+
+def Number_8():
         print("The eighth question is ")
         score = 0
         a = random.randint(0,99)
@@ -99,7 +104,8 @@ def Quiz_add():
         else:
             score += 0
         return score
-    elif Q9 == 9:
+
+def Number_9():
         print("The ninth question is ")
         score = 0
         a = random.randint(0,99)
@@ -111,8 +117,8 @@ def Quiz_add():
         else:
             score += 0
         return score
-    else:
-        Q10 == 10
+
+def Number_10():
         print("The tenth question is ")
         score = 0
         a = random.randint(0,99)
@@ -124,15 +130,19 @@ def Quiz_add():
         else:
             score += 0
         return score
-Q1 = 1
-Q2 = 2
-Q3 = 3
-Q4 = 4
-Q5 = 5
-Q6 = 6
-Q7 = 7
-Q8 = 8
-Q9 = 9
-Q10 = 10
 
-Quiz_add()
+def eval():
+    if 5 <= Result == 10:
+        print(f"Great Job",Name,"for completing the trainer")
+    else:
+        print(f"Better luck next time", Name, "Thank you!")
+
+print("WELCOME TO THE ADDITION TRAINER!")
+Name = input("First, what is your name?: ")
+
+print(f"Hello", Name, ",We have 10 questions for you to answer. Goodluck!")
+
+Result = int(Number_1() + Number_2() + Number_3()+ Number_4() + Number_5() + Number_6() + Number_7() + Number_8() + Number_9() + Number_10())
+
+print(f"You got {Result} out of 10")
+eval()
